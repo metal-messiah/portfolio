d45e1468c03af849ea527481aaedf05bd7ad7b0d
Jordan Porter <jordan@mtnra.com>
2019-02-11 09:57:58 -0700

    more cometS

--- classes/sphere.js
+++ classes/sphere.js
@@ -24,39 +24,56 @@ class Sphere {
 	draw() {
 		push();
 
-		normalMaterial();
+		try {
+			normalMaterial();
 
-		// Orange point light on the right
-		pointLight(150, 100, 0, 500, 0, 200);
+			// Orange point light on the right
+			pointLight(150, 100, 0, 500, 0, 200);
 
-		// Blue directional light from the left
-		directionalLight(0, 102, 255, -1, 0, 0);
+			// Blue directional light from the left
+			directionalLight(0, 102, 255, -1, 0, 0);
 
-		if (this.highlighted) {
-			// Red spotlight from the front
-			pointLight(255, 0, 0, 0, 0, 300);
-		} else {
-			// Yellow
-			pointLight(255, 255, 109, 0, 0, 300);
-		}
+			if (this.highlighted) {
+				// Red spotlight from the front
+				pointLight(255, 0, 0, 0, 0, 300);
+			} else {
+				// Yellow
+				pointLight(255, 255, 109, 0, 0, 300);
+			}
 
-		rotateZ(perspective.z);
-		rotateX(perspective.x);
-		rotateY(perspective.y);
+			rotateZ(perspective.z);
+			rotateX(perspective.x);
+			rotateY(perspective.y);
 
-		sphere(this.radius);
+			sphere(this.radius);
 
-		if (this.highlighted) {
-			// console.log('label: ', this.label);
-			fill('maroon');
-			noStroke();
-			textSize(16);
-			textFont(font);
-			textAlign(CENTER, CENTER);
-			rotateY(-perspective.y);
+			if (this.highlighted) {
+				// console.log('label: ', this.label);
+				fill('maroon');
+				noStroke();
+				textSize(16);
+				textFont(font);
+				textAlign(CENTER, CENTER);
+				rotateY(-perspective.y);
+				translate(-width / 2, -height / 2);
+
+				text(this.label, this.pos.x, this.pos.y + this.radius + 10);
+			}
+		} catch (err) {
 			translate(-width / 2, -height / 2);
+			ellipse(this.pos.x, this.pos.y, this.radius);
 
-			text(this.label, this.pos.x, this.pos.y + this.radius + 10);
+			if (this.highlighted) {
+				// console.log('label: ', this.label);
+				fill('maroon');
+				noStroke();
+				textSize(16);
+				textFont(font);
+				textAlign(CENTER, CENTER);
+				rotateY(-perspective.y);
+
+				text(this.label, this.pos.x, this.pos.y + this.radius + 10);
+			}
 		}
 		pop();
 	}
