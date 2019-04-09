1de9bcbcdbfece3331075cb78fd40dbfce05dc33
Jordan Porter <jordan@mtnra.com>
2019-04-08 18:36:00 -0600

    fixed scroll on mobile

--- sketch.js
+++ sketch.js
@@ -299,124 +299,33 @@ draw = () => {
 		sl.draw();
 	});
 
-	// sidebar.draw();
-
-
-	// let spacer = 30;
-	// let subspacer = 20;
-	// let topY = 25;
-
-	// fill('#444');
-	// rect(0, 0, 350, height);
-
-	// fill('orange');
-	// // translate(-width / 2, -height / 2);
-	// textFont(font);
-	// textSize(title);
-	// textAlign(CENTER, CENTER);
-
-	// text('Jordan Porter', leftX, topY);
-	// topY += spacer;
-	// textSize(content);
-	// fill('lightblue');
-	// text('Senior Software Developer', leftX, topY);
-	// topY += spacer + 15;
-
-	// image(headshot, 25, topY - 10, 300, 400)
-	// topY += spacer + 400;
-
-
-
-
-	// fill('orange');
-	// textSize(subtitle);
-	// text('Languages', leftX, topY);
-	// topY += spacer;
-
-	// fill('lightblue');
-	// textSize(subcontent);
-	// text('• HTML 5', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• JavaScript', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• Python', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• SQL', leftX + 5, topY);
-	// topY += spacer;
-
-	// fill('orange');
-	// textSize(subtitle);
-	// text('Frontend', leftX, topY);
-	// topY += spacer;
-
-	// fill('lightblue');
-	// textSize(subcontent);
-	// text('• Angular 6+', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• React', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• Electron', leftX + 5, topY);
-	// topY += spacer;
-
-	// fill('orange');
-	// textSize(subtitle);
-	// text('Backend', leftX, topY);
-	// topY += spacer;
-
-	// fill('lightblue');
-	// textSize(subcontent);
-	// text('• NodeJS', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• Flask', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• Django', leftX + 5, topY);
-	// topY += spacer;
-
-	// fill('orange');
-	// textSize(subtitle);
-	// text('Databases', leftX, topY);
-	// topY += spacer;
-
-	// fill('lightblue');
-	// textSize(subcontent);
-	// text('• PostgreSQL', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• MySQL', leftX + 5, topY);
-	// topY += subspacer;
-	// text('• MongoDB', leftX + 5, topY);
-	// topY += subspacer;
-
-	// push();
-	// texture(image);
-	// translate(width - 25, 50);
-	// plane(200, 200);
-	// pop();
 };
 
 mousePressed = (evt) => {
-	evt.preventDefault();
+	if (document.querySelector("#sidebar").style.left !== "0px" || displayWidth > 600) {
+		console.log("allowed")
+		if (orb.intersectsGeom(mouseX, mouseY, true)) {
+			shouldAnimate = new SmoothAnimation();
 
-	if (orb.intersectsGeom(mouseX, mouseY, true)) {
-		shouldAnimate = new SmoothAnimation();
+			shouldAnimate.frames = 90; // 1.5 second
+			shouldAnimate.yTarget = -0.9;
+			shouldAnimate.yRate = shouldAnimate.yTarget / shouldAnimate.frames;
+		}
 
-		shouldAnimate.frames = 90; // 1.5 second
-		shouldAnimate.yTarget = -0.9;
-		shouldAnimate.yRate = shouldAnimate.yTarget / shouldAnimate.frames;
-	}
-
-	menuItems.forEach((item) => {
-		if (item.index !== 3) {
-			if (item.intersectsGeom(mouseX, mouseY)) {
-				window.open(item.url, '_blank');
+		menuItems.forEach((item) => {
+			if (item.index !== 3) {
+				if (item.intersectsGeom(mouseX, mouseY)) {
+					window.open(item.url, '_blank');
+				}
 			}
-		}
-	});
+		});
 
-	socialLinks.forEach((sl) => {
-		if (sl.intersectsGeom(mouseX, mouseY)) {
-			window.open(sl.url, '_blank');
-		}
-	})
+		socialLinks.forEach((sl) => {
+			if (sl.intersectsGeom(mouseX, mouseY)) {
+				window.open(sl.url, '_blank');
+			}
+		})
+	}
 };
 
 mouseMoved = (evt) => {
